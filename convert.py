1c3875dff3ab16ae7634a933145342a745971d21
Javier Abadia <javi.abadia@gmail.com>
2014-01-05 16:18:51 +0100

    conversion script

---
+++ convert.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+import xlrd
+import csv
+
+def doSheet(sheet,year):
+	csvName = "d%04d.csv" % year
+	print sheet.name,"->", csvName;
+	with open( csvName, "w") as fp:
+		writer = csv.writer(fp,delimiter=';')
+		writer.writerow(["CODIGO","PROVINCIA","NOMBRE"])
+
+		titleRowIndex = 3
+		firstNameRowIndex = 5
+		for colIndex in range(1,sheet.ncols,3):
+			(code,provincia) = sheet.cell(titleRowIndex,colIndex).value.split(' - ')
+			name = sheet.cell(firstNameRowIndex,colIndex).value
+			writer.writerow([code,provincia.encode('utf-8'),name.encode('utf-8')])
+
+
+def main():
+	book = xlrd.open_workbook("nombres_por_fecha.xls")
+
+	sheetNames = [
+		# u"Indice",
+		# u"ESPAÑA_hombres",
+		# u"ESPAÑA_mujeres",
+		(u"NACIDOS ANTES DE 1930",1920),
+		(u"NACIDOS EN AÑOS 30",1930),
+		(u"NACIDOS EN AÑOS 40",1940),
+		(u"NACIDOS EN AÑOS 50",1950),
+		(u"NACIDOS EN AÑOS 60",1960),
+		(u"NACIDOS EN AÑOS 70",1970),
+		(u"NACIDOS EN AÑOS 80",1980),
+		(u"NACIDOS EN AÑOS 90",1990),
+		(u"NACIDOS EN AÑOS 2000",2000),
+		(u"NACIDOS EN AÑOS 2010",2010)
+	];
+
+	for (sheetName,year) in sheetNames:
+		sheet = book.sheet_by_name(sheetName);
+		doSheet(sheet,year);
+
+
+
+main();
